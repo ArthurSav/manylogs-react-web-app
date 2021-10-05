abe1c0cc3071973cd96c97e54394519f9d91ba8f
Arthur Saveliev <arthur.saveliev@gmail.com>
2021-10-05 20:56:54 +0200

    adds asset file for strings

---
+++ src/assets/text.js
@@ -0,0 +1,10 @@
+// App Dashboard
+
+export const txtAppDashboard = {
+  tipRecordTitle: "Record",
+  tipRecordMessage:
+    "Records Http traffic from your app and stores it into a data profile.",
+  tipReplayTitle: "Replay",
+  tipReplayMessage:
+    "Http responses in your app are replaced by previously recorded responses.",
+};
