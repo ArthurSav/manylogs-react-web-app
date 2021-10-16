684360e13728b49fd0c979ebb9990c05e2db0166
Arthur Saveliev <arthur.saveliev@gmail.com>
2021-10-17 00:49:57 +0200

    adds nav home

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,26 +1,29 @@
-import { Box, Button, Nav, Sidebar } from "grommet";
+import { Box, Button, Nav, Sidebar, Anchor } from "grommet";
 import { Chat, Projects, UserSettings } from "grommet-icons";
+import { Link } from "react-router-dom";
 import { HomeRounded } from "../assets/HomeRounded";
 
 const NavigationBar = () => {
   return (
-    <Sidebar responsive background="dark-2" footer={<SidebarFooter />}>
+    <Sidebar responsive={false} background="dark-2" footer={<SidebarFooter />}>
       <MainNavigation />
     </Sidebar>
   );
 };
 
+const MainNavigation = () => (
+  <Nav gap="small">
+    <Link to="/apps">
+      <Button icon={<HomeRounded />} />
+    </Link>
+    <Box border={{ color: "white", side: "bottom" }} />
+  </Nav>
+);
+
 const SidebarFooter = () => (
   <Nav gap="small">
     <Button icon={<Chat />} />
     <Button icon={<UserSettings />} />
-  </Nav>
-);
-
-const MainNavigation = () => (
-  <Nav gap="small">
-    <Button icon={<HomeRounded />} />
-    <Box border={{ color: "white", side: "bottom" }} />
   </Nav>
 );
 
