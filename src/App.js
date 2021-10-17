dd0c67145b4ce1c82920bd859ea81f4e30aece8e
Arthur Saveliev <arthur.saveliev@gmail.com>
2021-10-18 00:49:28 +0200

    adds private routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MyApps from "./pages/MyApps";
-import { Grommet } from "grommet";
+import { Grommet, Text } from "grommet";
 import PageLogin from "./pages/PageLogin";
 import { Switch, Route, Router, Redirect } from "react-router-dom";
 import { createBrowserHistory } from "history";
@@ -12,33 +12,53 @@ import PageSignup from "./pages/PageSignup";
 const hist = createBrowserHistory();
 
 function App() {
-  const auth = isAuthenticated();
   return (
     <Grommet full theme={mainTheme} themeMode="dark">
       <Router history={hist}>
         <Switch>
-          <Route path="/login" render={(props) => <PageLogin />} key="login" />
-          <Route exact path="/apps" render={(props) => <MyApps />} key="apps" />
           <Route
+            exact
+            path="/login"
+            render={(props) => <PageLogin />}
+            key="login"
+          />
+          <Route
+            exact
             path="/signup"
             render={(props) => <PageSignup />}
             key="signup"
           />
-          <Route
-            exact
-            path="/apps/:id"
-            render={(props) => <AppDashboard />}
-            key="app"
-          />
-          {auth ? (
-            <Redirect from="/" to="/apps" />
-          ) : (
-            <Redirect from="/" to="/login" />
-          )}
+          <PrivateRoute exact path="/" key="apps">
+            <MyApps />
+          </PrivateRoute>
+          <PrivateRoute exact path="/apps/:id" key="app">
+            <AppDashboard />
+          </PrivateRoute>
         </Switch>
       </Router>
     </Grommet>
   );
 }
+
+const PrivateRoute = ({ children, ...rest }) => {
+  let auth = isAuthenticated();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default App;
