1ffe5a57437a8ed0fa865d536cf9071492be6704
Arthur Saveliev <arthur.saveliev@gmail.com>
2021-10-17 18:58:24 +0200

    adds signup page WIP

---
+++ src/pages/PageSignup.js
@@ -0,0 +1,11 @@
+import { Heading, Main } from "grommet";
+
+const PageSignup = () => {
+  return (
+    <Main align="center">
+      <Heading>Signup Page</Heading>
+    </Main>
+  );
+};
+
+export default PageSignup;
